8323a1d11da128ee88b6af7c61c63387d6f74bab
Sean Briggs <seanrgbriggs@gmail.com>
2016-04-09 16:11:18 -0400

    They call me Shotgun, idk why tho

--- Assets/GunScripts/Gun.cs
+++ Assets/GunScripts/Gun.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using EZObjectPools;
 using EZEffects;
 
-public abstract class Gun : MonoBehaviour {
+public abstract class Gun : MonoBehaviour
+{
 
     public GameObject gunner;
 
@@ -14,7 +15,7 @@ public abstract class Gun : MonoBehaviour {
     protected float range;
 
     protected float spreadAngle;
-    
+
     public EffectImpact ImpactEffect;
     public EffectMuzzleFlash MuzzleEffect;
     public EffectTracer TracerEffect;
@@ -22,20 +23,20 @@ public abstract class Gun : MonoBehaviour {
 
     public AudioClip[] sounds;
 
-    public virtual void Start(){
+    public virtual void Start()
+    {
         gunner = GameObject.FindGameObjectWithTag("Gunner");
         curDelay = 0;
     }
 
-    public virtual void Update() {
+    public virtual void Update()
+    {
         curDelay = Mathf.Max(0, curDelay - Time.deltaTime);
         CasingEffect.enableEmission = Input.GetButton("Fire1");
     }
 
-    public virtual void Shoot() {
-        if (curDelay > 0)
-            return;
-        curDelay = delay;
+    public virtual void Shoot()
+    {
         if (sounds.Length > 0) AudioSource.PlayClipAtPoint(sounds[0], transform.position);
     }
 
@@ -45,7 +46,8 @@ public abstract class Gun : MonoBehaviour {
         direction.direction = Quaternion.Euler(Random.Range(-spreadAngle, spreadAngle), Random.Range(-spreadAngle, spreadAngle), 0) * cam.forward;
         RaycastHit info = new RaycastHit();
         MuzzleEffect.ShowMuzzleEffect(cam, true, null);
-        if (Physics.Raycast(direction, out info, range)) {
+        if (Physics.Raycast(direction, out info, range))
+        {
             GameObject target = info.transform.gameObject;
 
             TracerEffect.ShowTracerEffect(cam.position + direction.direction, direction.direction, info.distance);
@@ -54,21 +56,24 @@ public abstract class Gun : MonoBehaviour {
 
             if (target.GetComponent<DamageScript>() == null)
                 return;
-            if (target.CompareTag("Enemy")) {
+            if (target.CompareTag("Enemy"))
+            {
                 GetComponentInParent<GunnerScript>().ShowHitTime = delay * 0.8f;
                 target.GetComponent<DamageScript>().damage(damage);
             }
 
 
 
-        } else {
+        }
+        else {
 
             TracerEffect.ShowTracerEffect(cam.position + direction.direction, direction.direction, 100);
         }
 
     }
 
-    public float getDelay() {
+    public float getDelay()
+    {
         return curDelay;
     }
 }
--- Assets/GunScripts/Shotgun.cs
+++ Assets/GunScripts/Shotgun.cs
@@ -8,7 +8,7 @@ public class Shotgun : Gun{
     {
         damage = 10;
 
-        delay = 2;
+        delay = 1.5f;
         range = 400;
 
         spreadAngle = 30;
@@ -17,6 +17,9 @@ public class Shotgun : Gun{
     }
 
     public override void Shoot() {
+        if (curDelay > 0)
+            return;
+        curDelay = delay;
         base.Shoot();
         for (int i = 0; i < 25; i++) {
             SingleShot(new Ray(transform.position, transform.forward));
